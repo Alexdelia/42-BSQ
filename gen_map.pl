cae65ae3e5f00955352012452755225fed0aca08
adelille <adelille@student.42.fr>
2021-12-03 14:44:48 +0100

    update gen_map.pl

--- gen_map.pl
+++ gen_map.pl
@@ -3,11 +3,11 @@
 use warnings;
 use strict;
 
-die "program x y density" unless (scalar(@ARGV) == 3);
+die "Usage: perl [LEN_LINES] [NBR_LINES] [DENSITY]\n" unless (scalar(@ARGV) == 3);
 
 my ($x, $y, $density) = @ARGV;
 
-print "$y.ox\n";
+print "$y.oX\n";
 for (my $i = 0; $i < $y; $i++) {
 	for (my $j = 0; $j < $x; $j++) {
 		if (int(rand($y) * 2) < $density) {
