f7f46c376789b0d6fa283583e2c1fcce7b46a85f
Alexdelia <alexandre.delille.57@gmail.com>
2021-12-06 21:10:53 +0100

    fix opti of strjoin

--- srcs/utils/ft_strjoin_bsq.c
+++ srcs/utils/ft_strjoin_bsq.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin_free.c                                  :+:      :+:    :+:   */
+/*   ft_strjoin_bsq.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 22:26:06 by adelille          #+#    #+#             */
-/*   Updated: 2021/12/05 23:12:19 by adelille         ###   ########.fr       */
+/*   Updated: 2021/12/06 21:08:49 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	*ft_strjoin_bsq(char *s1, const int size1, char *s2, const int size2)
 	if (!res)
 		return (NULL);
 	ft_strcpy(res, s1, size1);
-	ft_strcpy(&res[ft_strlen(s1)], s2, size2);
+	ft_strcpy(&res[size1], s2, size2);
 	free(s1);
 	return (res);
 }
