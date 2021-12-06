a31923fd84ab04577ae2de3be443ded57dc264bd
Alexdelia <alexandre.delille.57@gmail.com>
2021-12-06 21:12:16 +0100

    more logical ft_ps file

--- srcs/utils/ft_pser.c
+++ srcs/utils/ft_pser.c
@@ -6,13 +6,13 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 19:43:42 by adelille          #+#    #+#             */
-/*   Updated: 2021/12/05 21:02:25 by adelille         ###   ########.fr       */
+/*   Updated: 2021/12/06 21:11:06 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/bsq.h"
 
-int	ft_strlen(const char *str)
+static int	ft_strlen(const char *str)
 {
 	int	i;
 
